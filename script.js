e6939a062bebc6f0a5fd04095d3e801d2c15d091
chrvstopher19 <140680259+chrvstopher19@users.noreply.github.com>
2023-10-17 11:19:57 -0400

    completed 2.5/3

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
 const container = document.querySelector("container")
 const rolleDice = document.getElementById("rDice")
+const resetGame = document.getElementById("reset")
 const results =  document.getElementById("Results")
 const player1Img = document.getElementById("player1-img")
 const compImg = document.getElementById("comp-img")
 
-let playeRoll, compRoll, rounds
-//
+let playerWins = 0;
+let compWins = 0;
+let rounds = 0;
+
 rolleDice.addEventListener("click", () => {
-    playeRoll = diceRoll();
-    compRoll = diceRoll();
+    if(rounds < 3){
+        playeRoll = diceRoll();
+        compRoll = diceRoll();
 
-    player1Img.src = `./images/dice${playeRoll}`
-    compImg.src = `./images/dice${compRoll}`
+        player1Img.src = `./images/dice${playeRoll}.png`
+        compImg.src = `./images/dice${compRoll}.png`
 
-    if(playeRoll > compRoll) {
-        results.textContent = "YOU WIN!!"
-    }else if(playeRoll < compRoll){
-        results.textContent = "YOU LOSE!!"
-    }else if(playeRoll === compRoll){
-        results.textContent = "IT'S DRAW.."
+        if (playeRoll > compRoll) {
+            results.textContent = "YOU WIN THIS ROUND!!";
+            playerWins++;
+        } else if (playeRoll < compRoll){
+            results.textContent = "YOU LOSE THIS ROUND!!"
+            compWins++;
+        } else if (playeRoll === compRoll){
+            results.textContent = "IT'S DRAW THIS ROUND"
+        }
+
+        rounds++;
+    }
+
+    if(rounds === 3) {
+        console.log("im in here")
+        if(playerWins > compWins) {
+            results.textContent = "YOU WIN THE GAME!!";
+        }else if (playerWins < compWins) {
+            results.textContent = "COMPUTER WINS!!"
+        } else {
+            results.textContent = "IT'S A DRAW.."
+        }
     }
 });
 
@@ -26,3 +46,14 @@ const diceRoll = function () {
     const result = Math.ceil(Math.random() * 6);
     return result
 }
+
+resetGame.addEventListener("click", () => {
+    playerWins = 0;
+    compWins = 0;
+    rounds = 0;
+
+    player1Img.src = './images/animatedDice.gif'
+    compImg.src = './images/animatedDice.gif'
+
+    results.textContent = "Results";
+})
